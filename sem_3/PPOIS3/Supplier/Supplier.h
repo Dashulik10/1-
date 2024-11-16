5d5789972834a1f3a94904a7e6c2350fe6c2f356
Dashulik10 <144939580+Dashulik10@users.noreply.github.com>
2024-11-16 14:01:18 +0300

    Update Supplier.h

--- sem_3/PPOIS3/Supplier/Supplier.h
+++ sem_3/PPOIS3/Supplier/Supplier.h
@@ -13,12 +13,12 @@ class Supplier : protected Person
 {
 private:
 	string name_of_supplier;
-	ContactInfo contInf_of_supplier;
+	Contact_Info contInf_of_supplier;
 	string category_of_goods;
 	double rating_of_supplier;
 public:
 	Supplier();
-	Supplier(const string& name, const ContactInfo& contact, const string& category, double rating);
+	Supplier(const string& name, const Contact_Info& contact, const string& category, double rating);
 
 	string getNameOfSupplier() const;
 	ContactInfo getContactInfo() const;
